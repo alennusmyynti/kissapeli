aaff06ba66b4b176557a5e71fe5de8137818e06c
= <kodarit.ikonen@gmail.com>
2023-10-06 18:14:49 +0300

    random kortteja

--- kissapeli/src/App.jsx
+++ kissapeli/src/App.jsx
@@ -1,11 +1,16 @@
 
 import Card from "./components/card";
 import './App.css';
+import { useState } from "react";
+
+
+
+const getRandomInt=(min,max) => Math.floor(min+(max-min+1)*Math.random())
 
 const playerCard=[
   {
     type:"card-player",
-    image:"http://placekitten.com/120/100?image=0",
+    image:"http://placekitten.com/120/103?image=0",
     stats:[
       {name:'Cuteness', value: 3+Math.round(5*Math.random())},
       {name:'Power', value:5+Math.round(3*Math.random())},
@@ -13,9 +18,21 @@ const playerCard=[
       {name:'Speed', value:3+Math.round(3*Math.random())},
     ],
     title:'Cat-astrophe',
+},
+{
+  type:"card-player",
+  image:"https://placekitten.com/120/103?image=7",
+  stats:[
+    {name:'Cuteness', value: 3+Math.round(5*Math.random())},
+    {name:'Power', value:5+Math.round(3*Math.random())},
+    {name:'Vitality', value:8+Math.round(8*Math.random())},
+    {name:'Speed', value:3+Math.round(3*Math.random())},
+  ],
+  title:'Catsparow',
 }
-
 ]
+
+
 
 const enemyCard=[
   {
@@ -28,25 +45,68 @@ const enemyCard=[
       {name:'Speed', value:3+Math.round(3*Math.random())},
     ],
     title:'Mr Pawsome',
+},
+{
+  type:"card-enemy",
+  image:"https://placekitten.com/120/102?image=10",
+  stats:[
+    {name:'Cuteness', value: 3+Math.round(5*Math.random())},
+    {name:'Power', value:5+Math.round(3*Math.random())},
+    {name:'Vitality', value:8+Math.round(8*Math.random())},
+    {name:'Speed', value:3+Math.round(3*Math.random())},
+  ],
+  title:'Ka-Paw',
+},
+{
+  type:"card-enemy",
+  image:"https://placekitten.com/120/103?image=15",
+  stats:[
+    {name:'Cuteness', value: 3+Math.round(5*Math.random())},
+    {name:'Power', value:5+Math.round(3*Math.random())},
+    {name:'Vitality', value:8+Math.round(8*Math.random())},
+    {name:'Speed', value:3+Math.round(3*Math.random())},
+  ],
+  title:'RamPaw G',
 }
 
 ]
 
+var playerid=getRandomInt(0,playerCard.length-1)
+var enemyid=getRandomInt(0,enemyCard.length-1)
+
+const cards=[playerCard,playerid,enemyCard,enemyid]
+
+const createCard=(index,side)=>({
+  type:cards[2*side][2*side+1].type,
+  image:cards[2*side][2*side+1].image,
+  stats:[
+    {name:'Cuteness', value: 3+Math.round(5*Math.random())},
+    {name:'Power', value:5+Math.round(3*Math.random())},
+    {name:'Vitality', value:8+Math.round(8*Math.random())},
+    {name:'Speed', value:3+Math.round(3*Math.random())},
+  ],
+  title:cards[2*side][2*side+1].title,
+  id:crypto.randomUUID()
+})
+
 
 export default function app(){
+  const[result,SetResult] = useState()
 
 function compareCards(){
-  const playerstats=playerCard[0].stats
-  const enemystats=enemyCard[0].stats
+  const playerstats=playerCard[playerid].stats
+  const enemystats=enemyCard[enemyid].stats
+
+
 
   var playerturns=0
   var enemyturns=0
-  if (enemystats[3].value*( playerstats[1].value-enemystats[0].value)>0){
+  if (( playerstats[1].value-enemystats[0].value)>0){
    playerturns=Math.ceil( enemystats[2].value/(playerstats[3].value/enemystats[3].value*( playerstats[1].value-enemystats[0].value)))
 }else{
    playerturns=Math.ceil( enemystats[2].value/(playerstats[3].value))
 }
-if (playerstats[3].value*(enemystats[1].value-playerstats[0].value)>0){
+if ((enemystats[0].value-playerstats[1].value)>0){
    enemyturns=Math.ceil( playerstats[2].value/(enemystats[3].value/playerstats[3].value*(enemystats[1].value-playerstats[0].value)))
 }else{
    enemyturns=Math.ceil( playerstats[2].value/(enemystats[3].value))
@@ -56,17 +116,28 @@ if (playerstats[3].value*(enemystats[1].value-playerstats[0].value)>0){
 
 
   if (playerturns>enemyturns){
-    console.log(enemyCard[0].title+ " won in "+enemyturns+" turns!");
+    SetResult(enemyCard[enemyid].title+ " won in "+enemyturns+" turns!")
+     console.log(enemyCard[enemyid].title+ " won in "+enemyturns+" turns!");
+    
 
   }else if(playerturns<enemyturns){
-    console.log(playerCard[0].title+ " won in "+playerturns+" turns!");
+    SetResult(playerCard[playerid].title+ " won in "+playerturns+" turns!");
+ console.log(playerCard[playerid].title+ " won in "+playerturns+" turns!");
 
 
   }else{
-    console.log("Draw! The fight lasted for "+playerturns+" turns!")
+     SetResult("Draw! The fight lasted for "+playerturns+" turns!")
+      console.log("Draw! The fight lasted for "+playerturns+" turns!");
+
   }
-  
- 
+  console.log([
+    Math.ceil( enemystats[2].value/(playerstats[3].value/enemystats[3].value*( playerstats[1].value-enemystats[0].value))),
+    Math.ceil( enemystats[2].value/(playerstats[3].value)),
+    Math.ceil( playerstats[2].value/(enemystats[3].value/playerstats[3].value*(enemystats[1].value-playerstats[0].value))),
+    Math.ceil( playerstats[2].value/(enemystats[3].value)),
+    enemystats, playerstats,playerid
+   ])
+
 
 }
 
@@ -74,11 +145,15 @@ if (playerstats[3].value*(enemystats[1].value-playerstats[0].value)>0){
 
 return(
 <div>
+  
   <h1 className="game">Pawsome Pawlers</h1>
   <div className="game">
-  <Card card={playerCard[0]}/>
+  <Card card={playerCard[playerid]}/>
+  <div className="center-area">
+  <p>{result || 'Presss the button'}</p>
   <button type="button" onClick={compareCards}>Fight!</button>
-  <Card card={enemyCard[0]}/>
+  </div>
+  <Card card={enemyCard[enemyid]}/>
 
  
   </div>
